2201fdd3d04e0558c84870ff12c984d6b2376612
Marco Laptop <mpoon25@my.bcit.ca>
2022-11-15 09:45:07 -0800

    funcition practice

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+// function greet(username){
+//     console.log(`hewwo ${username}`);
+// }
+
+// const greet = (username) =>{
+//     console.log(`hewwo ${username}`);
+// }
+
+const greet = function(user){
+    console.log(`hewwo ${username}`);
+}
+
+let username = prompt('What is your name?')
+greet(username)
